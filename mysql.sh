302aae78a5f7f42ad1e87ec80b548b43d3838f6e
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-15 01:02:45 +0530

    set

--- mysql.sh
+++ mysql.sh
@@ -7,14 +7,14 @@ check_root
 echo "Please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL Server"
+dnf install mysql-serverrr -y &>>$LOGFILE
+# VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+# VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+# VALIDATE $? "Starting MySQL Server"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
@@ -25,7 +25,7 @@ mysql -h dbsql.naga.website -uroot -p${mysql_root_password} -e 'show databases;'
 if [ $? -ne 0 ]
 then
   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-  VALIDATE $? "MySQL Root password Setup"
+  # VALIDATE $? "MySQL Root password Setup"
 else
   echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
