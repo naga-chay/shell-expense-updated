50657b21a8b4738e341cf44e31181d4edd6d2d71
Naga Chaitanya <ballalanagachaitanya@gmail.com>
2025-04-15 02:37:30 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 handle_error(){
     echo "Error occured at line number: $1, error command: $2"
@@ -17,24 +17,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
 VALIDATE(){
-
-    if [ $1 -ne 0 ]
-    then 
-         echo -e "$2...$R FAILURE $N"   
-         exit 1
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
     else
-         echo -e "$2... $G SUCCESS $N"
-    fi   
-} 
-
-check_root(){
-if [ $USERID -ne 0 ]
-then 
-         echo "Please run this script with root access."
-         exit 1 # manually exit if error comes.
-else
-         echo "you are super user."
-fi
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
-
